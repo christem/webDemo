299e2b2087e496c7d7d5134d2cc1ce2790c72d49
suny <suny@YX-87.yanxintec.com>
2016-11-09 20:51:22 +0800

    no message

--- src/main/java/org/smart4j/chapter3/aspect/ControllerAspect.java
+++ src/main/java/org/smart4j/chapter3/aspect/ControllerAspect.java
@@ -1,5 +1,4 @@
-package org.smart4j.chapter4.aspect;
-
+package org.smart4j.chapter3.aspect;
 import java.lang.reflect.Method;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,12 +24,22 @@ public class ControllerAspect extends AspectProxy {
         LOGGER.debug("---------- begin ----------");
         LOGGER.debug(String.format("class: %s", cls.getName()));
         LOGGER.debug(String.format("method: %s", method.getName()));
+
+        System.out.println("---------- begin ----------");
+        System.out.println(String.format("class: %s", cls.getName()));
+        System.out.println(String.format("method: %s", method.getName()));
+
         begin = System.currentTimeMillis();
+
     }
 
     @Override
     public void after(Class<?> cls, Method method, Object[] params, Object result) throws Throwable {
-        LOGGER.debug(String.format("time: %dms", System.currentTimeMillis() - begin));
+        long time = System.currentTimeMillis() - begin;
+        LOGGER.debug(String.format("time: %dms", time));
         LOGGER.debug("----------- end -----------");
+
+        System.out.println(String.format("time: %dms", time));
+        System.out.println("----------- end -----------");
     }
 }
